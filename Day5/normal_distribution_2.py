3777ea8ab84490ed14ae713a943a84fa4ea2d262
Gunal-k <gunalk.0007@gmail.com>
2025-02-25 14:37:16 +0530

    Remove unnecessary blank lines in normal_distribution_2.py

--- Day5/normal_distribution_2.py
+++ Day5/normal_distribution_2.py
@@ -11,8 +11,6 @@ prob1 = 1 - normal_cdf(a, mean, sd)
 prob2 = 1 - normal_cdf(b, mean, sd)
 prob3 = normal_cdf(b,mean,sd)
 
-
-
 print(round(prob1*100, 2))
 print(round(prob2*100, 2))
 print(round(prob3*100, 2))
