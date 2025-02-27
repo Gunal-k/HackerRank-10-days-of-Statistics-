8192f0c9545e60365f368ff373dcf667fbedf432
Gunal-k <gunalk.0007@gmail.com>
2025-02-27 14:20:08 +0530

    Add Spearman rank correlation function with user input handling

--- Day7/pearson_correlation_1.py
+++ Day7/pearson_correlation_1.py
@@ -1,1 +1,16 @@
- 
+import math
+def pearson_correlation(n,X, Y):
+    mean_X = sum(X) / n
+    mean_Y = sum(Y) / n
+
+    num = sum((X[i] - mean_X) * (Y[i] - mean_Y) for i in range(n))
+    den_X = math.sqrt(sum((X[i] - mean_X) ** 2 for i in range(n)))
+    den_Y = math.sqrt(sum((Y[i] - mean_Y) ** 2 for i in range(n)))
+
+    return num / (den_X * den_Y)
+
+n = int(input())
+mean_X = list(map(float,input().split()))
+mean_Y = list(map(float,input().split()))
+
+print(round(pearson_correlation(n,mean_X,mean_Y),3))
--- Day7/spearman_rank.py
+++ Day7/spearman_rank.py
@@ -1,1 +1,21 @@
- 
+def rank(data):
+    sorted_data = sorted((x, i) for i, x in enumerate(data))
+    ranks = [0] * len(data)
+    for rank, (_, i) in enumerate(sorted_data, start=1):
+        ranks[i] = rank
+    return ranks
+
+def spearman_rank_correlation(X, Y):
+    n = len(X)
+    rank_X = rank(X)
+    rank_Y = rank(Y)
+    d_squared = [(rank_X[i] - rank_Y[i]) ** 2 for i in range(n)]
+    
+    rho = 1 - (6 * sum(d_squared)) / (n * (n**2 - 1))
+    return rho
+
+_ = int(input())
+X = list(map(float,input().split()))
+Y = list(map(float,input().split()))
+
+print(round(spearman_rank_correlation(X,Y),3))
