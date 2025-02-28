08ce0c2c76ba098b47a1921581cc82883104b913
Gunal-k <gunalk.0007@gmail.com>
2025-02-28 13:55:00 +0530

    Add least squares regression line function with user input handling

--- Day8/least_squares.py
+++ Day8/least_squares.py
@@ -1,1 +1,25 @@
- 
+def LSRL(X,Y):
+    n = 5
+    mean_X = sum(X)/n
+    mean_Y = sum(Y)/n
+    
+    numerator = sum((X[i] - mean_X)*(Y[i]-mean_Y) for i in range(n))
+    denominator = sum((X[i] - mean_X)** 2 for i in range(n))
+    
+    b = numerator/denominator
+    a = mean_Y- b* mean_X
+    
+    return a,b
+
+X,Y=[],[]
+for _ in range(5):
+    x, y = map(int, input().split())
+    X.append(x)
+    Y.append(y)
+
+a, b =LSRL(X,Y)
+
+x_pred = 80
+y_pred = a + b*x_pred
+
+print(round(y_pred,3))
